bb9fc68d13a18b29cd8075bf2b7857afe08f0332
santhrupthi shetty <santhrupthis@infinite.com>
2021-08-04 11:10:38 +0530

    first ccommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularPro';
+  constructor(private modalService: NgbModal) {}
+
 }
